2280bdaaab2013cf6b4ae7f6ef226367b279e97c
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-19 14:28:23 +0200

    Add files via upload

---
+++ cw2.2/cw2.2/Osoba.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2._2
+{
+    class Osoba
+    {
+        public string imie;
+        public string nazwisko;
+        public int rokUrodzenia;
+        public int waga;
+        public int wzrost;
+        public bool okulary;
+        public string plec;
+
+        public void ObliczWiek()
+        {
+            Console.WriteLine((DateTime.Now.Year) - rokUrodzenia);
+        }
+
+        public void WypiszInfo()
+        {
+            if (plec == "K")
+            {
+                Console.WriteLine("Dyrektorem jest Pani {0} {1} ",imie,nazwisko);
+            }
+            else if (plec == "M")
+            {
+                Console.WriteLine("Dyrektorem jest Pan {0} {1} ", imie, nazwisko);
+            }
+
+        }
+
+    }
+}
---
+++ cw2.2/cw2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba dyrektor = new Osoba();
+            dyrektor.imie = "janusz";
+            dyrektor.nazwisko = "KOwalski";
+            dyrektor.rokUrodzenia = 1964;
+            dyrektor.plec = "M";
+            dyrektor.ObliczWiek();
+            dyrektor.WypiszInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
